a23afe9eaa7253e3c97ad246debea3424829ae17
PhilipOndieki <philipbarongo30@gmail.com>
2025-10-19 11:27:27 +0300

    first commit

---
+++ src/components/StudentCard.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+export default function StudentCard({ student, onEdit, onDelete}){
+    const [edit, setEdit] = useState(false);
+    const [draft, setDraft] = useState(student);
+
+    return(
+        <div className='rounded-xl border bg-white p-4 shadow-sm'>
+            {!edit ? (
+                <div className='flex justify-beteween'>
+                    <div>
+                        <h3 className='text-lg font-semibold'>{student.name}</h3>
+                        <p className='text-slate-600 text-sm'>{student.email}</p>
+                        <p className='text-slate-600 text-sm'>Age: {student.age}</p>
+                    </div>
+                    <div classname='flex gap-2'>
+                        <button onCLick={()=> setEdit(true)} className='border px-3 py-1 rounded-lg text-sm'>Edit</button>
+                        <button onCLick={()=> onDelete(student._id)} className='bg-red-600 text-white px-3 py-1 rounded-lg text-sm'>Delete</button>
+                    </div>
+                </div>
+            ):(
+                <form>
+                    <input 
+                        className='border rounded-lg px-3 py-1 w-full'
+                        value={draft.name}
+                        onChange={e=> setDraft ({ ...draft, name:e.target.value})}/>
+
+                    <input 
+                    className='border rounded-lg px-3 py-1 w-full'
+                    value={draft.name}
+                    onChange={e=> setDraft ({ ...draft, email:e.target.value})}/>
+
+                    <input 
+                    className='border rounded-lg px-3 py-1 w-full'
+                    value={draft.name}
+                    onChange={e=> setDraft ({ ...draft, age:Number(e.target.value)})}/>
+
+                    <div>
+                        <button className='bg-green-600 text-white px-3 py-1 rounded-lg text-sm'>Save</button>
+                        <button type='button' onClick={()=>setEdit(false)} className='border px-3 py-1 '>Cancel</button>
+                    </div>
+
+                </form>
+            )}
+
+        </div>
+    );
+}
---
+++ src/components/StudentForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+export default function StudentForm(){
+    const [form, setForm] = useState({name: '', email: '', age: ''});
+    
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setForm(f=> ({ ...f, [name]: value}));
+    }
+    //f = {name: '', email: '', age: ''} - previous state of our form 
+    // ...f = copies and pastes the previous state of the form 
+    // f = {name: 'Philip', email: 'philipbarongo30@gmail.com', age: '24'}
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!form.name || !form.email || !form.age) return;
+
+        onSubmit({...form, age:Number(form.age)});
+        setForm({ name: '', email: '', age: ''})
+    };
+
+
+    return(
+        <form onSubmit={handleSubmit} className='rounded-xl border bg-white p-4 flex flex-wrap gap-2'>
+            <input 
+                name='name'
+                value={form.name}
+                onChange={handleChange}
+                placeholder='Name'
+                className='border rounded-lg px-3 py-2 flex-1'/>
+                
+            <input 
+                name='name'
+                value={form.email}
+                onChange={handleChange}
+                placeholder='Email'
+                className='border rounded-lg px-3 py-2 flex-1'/>
+
+            <input
+                name='name'
+                value={form.age}
+                onChange={handleChange}
+                placeholder='Age'
+                className='border rounded-lg px-3 py-2 flex-1'/>
+                
+                <button className='bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700'></button>
+        </form>
+    );
+}
